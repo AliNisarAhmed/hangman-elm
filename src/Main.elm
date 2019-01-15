404fa20af7269f3391a6a22eed82c5fd46f10c9c
AliNisarAhmed <ali.nisar87@gmail.com>
2019-01-16 00:23:38 +0500

    letter unveiling on userinput and wrongLetters List done, reset button working, to add: win & lose condition detection

--- src/Main.elm
+++ src/Main.elm
@@ -13,6 +13,10 @@ type alias LetterObj =
     }
 
 
+
+-- Helper Functions
+
+
 mapToLetterObj : String -> List LetterObj
 mapToLetterObj str =
     str
@@ -37,6 +41,43 @@ showLetter obj =
             "*"
 
 
+mapLetterObjToWord : List LetterObj -> List String
+mapLetterObjToWord letterObj =
+    List.map (\obj -> obj.letter) letterObj
+
+
+checkGuess : Model -> ( Model, Cmd Msg )
+checkGuess model =
+    let
+        word =
+            mapLetterObjToWord model.word
+    in
+    if
+        List.member model.guess word
+        -- if the user has guessed a letter correctly
+        -- then we need to unveil that letter
+    then
+        -- unveil the letter
+        -- unveilLetter was provided to List.map as partially applied with model, await LetterObj from the map
+        ( { model | word = List.map (unveilLetter model) model.word, guess = "" }
+        ,-- TODO: Cmd CheckForWin
+        )
+
+    else
+        ( { model | wrongLetters = List.append model.wrongLetters (List.singleton model.guess), guess = "" }
+        , Cmd.none
+        )
+
+
+unveilLetter : Model -> LetterObj -> LetterObj
+unveilLetter model letterObj =
+    if letterObj.letter == model.guess then
+        { letterObj | isShowing = True }
+
+    else
+        letterObj
+
+
 
 ---- MODEL ----
 
@@ -44,6 +85,8 @@ showLetter obj =
 type alias Model =
     { guess : String
     , word : List LetterObj
+    , wrongLetters : List String
+    , gameStatus : String
     }
 
 
@@ -51,6 +94,8 @@ init : ( Model, Cmd Msg )
 init =
     ( { guess = ""
       , word = mapToLetterObj Word.word
+      , wrongLetters = []
+      , gameStatus = "Take a Guess"
       }
     , Cmd.none
     )
@@ -63,6 +108,8 @@ init =
 type Msg
     = WordInput String
     | FormSubmit
+    | ResetGame
+    | CheckForWin
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -74,9 +121,30 @@ update msg model =
             )
 
         FormSubmit ->
-            ( { model | guess = "" }
+            checkGuess model
+
+        ResetGame ->
+            ( { model | word = mapToLetterObj "react", guess = "", wrongLetters = [] }
             , Cmd.none
             )
+
+        CheckForWin ->
+            let
+                result =
+                    model.word
+                        |> List.map (\obj -> obj.isShowing)
+                        |> List.all (\x -> x == True)
+            in
+            case result of
+                True ->
+                    ( { model | gameStatus = "You won!!!" }
+                    , Cmd.none
+                    )
+
+                False ->
+                    ( { model | gameStatus = "You lost, lol" }
+                    , Cmd.none
+                    )
 
 
 
@@ -87,15 +155,15 @@ view : Model -> Html Msg
 view model =
     div [ class "App" ]
         [ h1 [ id "title" ] [ text "Hangman Game" ]
-        , div [ class "msg" ] [ text "Take a Guess" ]
-        , div [] [ text (displayWord model.word) ]
-        , div [] [ text "WrongLetters component here" ]
+        , div [ class "msg" ] [ text model.gameStatus ]
+        , div [ class "wordDisplay" ] [ text (displayWord model.word) ]
+        , div [ class "wrongLetters" ] [ text (String.concat model.wrongLetters) ]
         , div [ class "guessesLeft" ]
             [ p [] [ text "Guesses Left: 5" ] ]
         , Html.form [ onSubmit FormSubmit ]
-            [ input [ type_ "text", name "guess", required True, onInput WordInput ] []
+            [ input [ type_ "text", name "guess", required True, onInput WordInput, value model.guess ] []
             ]
-        , button [ class "btn" ] [ text "New Word" ]
+        , button [ class "btn", onClick ResetGame ] [ text "New Word" ]
         ]
 
 
